28c64421135ee83d8547c53dddfa7c711bc70bea
Claudia Maciel <clc_80@yahoo.com>
2020-07-07 20:06:37 -0700

    Further improved performance by reusing the same CIImage instead of creating a new one each time

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -23,12 +23,15 @@ class PhotoFilterViewController: UIViewController {
             scaledSize = CGSize(width: scaledSize.width * scale,
                                 height: scaledSize.height * scale)
             
-            let scaledUIImage = originalImage.imageByScaling(toSize: scaledSize)
-            scaledImage = scaledUIImage
+            guard let scaledUIImage = originalImage.imageByScaling(toSize: scaledSize) else {
+                scaledImage = nil
+                return
+            }
+            scaledImage = CIImage(image: scaledUIImage)
         }
     }
     
-    var scaledImage: UIImage? {
+    var scaledImage: CIImage? {
         didSet {
             updateImage()
         }
@@ -47,16 +50,15 @@ class PhotoFilterViewController: UIViewController {
 
 	}
     
-    private func image(byFiltering image: UIImage) -> UIImage {
-        let inputImage = CIImage(image: image)
+    private func image(byFiltering inputImage: CIImage) -> UIImage? {
         
         filter.inputImage = inputImage
         filter.saturation = saturationSlider.value
         filter.brightness = brightnessSlider.value
         filter.contrast = contrastSlider.value
         
-        guard let outputImage = filter.outputImage else { return image }
-        guard let renderedCGImage = context.createCGImage(outputImage, from: outputImage.extent) else { return image }
+        guard let outputImage = filter.outputImage else { return nil }
+        guard let renderedCGImage = context.createCGImage(outputImage, from: outputImage.extent) else { return nil }
         
         return UIImage(cgImage: renderedCGImage)
         
